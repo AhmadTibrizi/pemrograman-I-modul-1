5e90cf86aa4b1368c53487f409dd78aacd4e771d
Riez69 <ahmadbarangaja12@gmail.com>
2025-09-24 12:32:37 +0800

    Menambahkan prak005

---
+++ C/PRAK005-2510817210027-Ahmad_Tibrizi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Huruf A
+    printf("   #      ##### \n");
+    printf("  # #       #   \n");
+    printf(" #   #      #   \n");
+    printf(" #####      #   \n");
+    printf(" #   #      #   \n");
+    printf(" #   #    ##### \n");
+    printf("\n");
+    return 0;
+}
