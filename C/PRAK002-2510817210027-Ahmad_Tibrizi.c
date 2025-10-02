367fc8a7015be5e90b3dcd8ee86472ce7262905b
Riez69 <ahmadbarangaja12@gmail.com>
2025-10-02 14:19:56 +0800

    fix issue

--- C/PRAK002-2510817210027-Ahmad_Tibrizi.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Selamat Pagi, Ahmad Tibrizi\n");
-    printf("Selamat Siang, Ahmad Tibrizi\n");
-    printf("Selamat Malam, Ahmad Tibrizi\n");
-    return 0;
-}
---
+++ c/PRAK101-2510817210027-Ahmad_Tibrizi.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+    int x = 5;
+    int y = 7;
+    int z = 9;
+    int res = x + y + z;
+    printf("Jumlah variabel tersebut adalah %d\n", res);
+}
