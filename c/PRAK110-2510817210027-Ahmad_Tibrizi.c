74faae80ba19c5c93a36932a62f37ab3c0e65ec0
Riez69 <ahmadbarangaja12@gmail.com>
2025-10-07 11:34:18 +0800

    update

--- c/PRAK110-2510817210027-Ahmad_Tibrizi.c
+++ c/PRAK110-2510817210027-Ahmad_Tibrizi.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
-    // Diketahui
+void main() {
     int alas = 5;
     int tinggi = 12;
-
-    // Perhitungan
     double sisi_miring = sqrt(alas * alas + tinggi * tinggi);
     int keliling = alas + tinggi + (int)sisi_miring;
     int luas = (alas * tinggi) / 2;
-
-    // Menampilkan hasil
     printf("Diketahui :\n");
     printf("Alas = %d cm\n", alas);
     printf("Tinggi = %d cm\n\n", tinggi);
-    
     printf("Jawab :\n");
     printf("Sisi A = %d cm\n", alas);
     printf("Sisi B = %d cm\n", tinggi);
     printf("Sisi C = %.0f cm\n", sisi_miring);
     printf("Keliling = %d cm\n", keliling);
     printf("Luas = %d cm\n", luas);
-
-    return 0;
 }
